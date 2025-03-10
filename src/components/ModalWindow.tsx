f8468ebe876ee5ee5e82e7e36b8939fa5b0b4b68
kinako1415 <Owata09@outlook.jp>
2025-03-10 22:12:03 +0900

    modalの追加

---
+++ src/components/ModalWindow.tsx
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+import styles from "./ModalWindow.module.scss";
+import { ReactNode } from "react";
+
+type ModalWindowType = {
+  children: ReactNode;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+};
+
+const ModalWindow: React.FC<ModalWindowType> = ({
+  children,
+  setIsOpen,
+  isOpen,
+}) => {
+  const handleBackgroundClick = () => {
+    // 背景がクリックされたときにモーダルを閉じる
+    setIsOpen(false);
+  };
+  return (
+    <>
+      {isOpen ? (
+        <div className={styles.container} onClick={handleBackgroundClick}>
+          <motion.div
+            className={styles.mainContainer}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: "100%", scale: "100%" }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: "100%", scale: "100%" }}
+          ></motion.div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default ModalWindow;
