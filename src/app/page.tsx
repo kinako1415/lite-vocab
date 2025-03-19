fa371e8b863dafd89d7106e1b5c3b818735515ef
kinako1415 <Owata09@outlook.jp>
2025-03-19 16:10:19 +0900

    firebaseの初期化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import StartButton from "@/components/StartButton";
-
 export default function Home() {
-  return (
-    <div>
-      <StartButton></StartButton>
-    </div>
-  );
+  return <div></div>;
 }
