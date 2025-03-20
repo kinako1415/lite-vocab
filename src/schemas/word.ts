71f51144f81988a34744bc25718a044d97069801
kinako1415 <Owata09@outlook.jp>
2025-03-20 10:44:26 +0900

    wordの型を追加

---
+++ src/schemas/word.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const wordSchemas = z.object({
+  createdAt: z.string(),
+  meaning: z
+    .string()
+    .min(1, "意味を追加してください")
+    .max(50, "50文字以下にしてください"),
+  word: z
+    .string()
+    .min(1, "単語を追加してください")
+    .max(50, "50文字以下にしてください"),
+});
