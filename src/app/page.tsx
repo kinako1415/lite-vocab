0e1d9df737afa67f81df6fdd6a936c571e5e00f0
kinako1415 <Owata09@outlook.jp>
2025-03-22 19:05:56 +0900

    ユーザー登録の作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
+import { CreateUser } from "@/lib/auth";
 import { addWord } from "@/lib/firestore";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
   return (
     <div>
       <button
@@ -11,6 +16,25 @@ export default function Home() {
       >
         単語の追加！！
       </button>
+      <form
+        onSubmit={() => {
+          CreateUser({ email, password });
+        }}
+      >
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">ユーザー登録</button>
+      </form>
     </div>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,27 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+export const CreateUser = ({
+  email,
+  password,
+}: {
+  email: string | undefined;
+  password: string | undefined;
+}) => {
+  if (email === undefined || password == undefined) {
+    throw new Error("emailまたはpasswordが未設定です。流石に設定しろ!!!!!");
+  }
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.log(errorCode, errorMessage);
+    });
+};
