036feb8d084d8cb2a50f014743c680306d73d33b
kinako1415 <Owata09@outlook.jp>
2025-03-26 18:06:20 +0900

    クッキーセッションの作成

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,28 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/verifyToken`,
+  const session = req.cookies.get("session")?.value;
+  const isLoggedIn = session;
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_API_URL}/login`);
+  }
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/verify`,
     {
       method: "GET",
-      headers: { Cookie: req.headers.get("cookie") || "" },
+      headers: {
+        Cookie: `session=${session}`,
+      },
     }
   );
 
-  if (res.status === 200) {
-    const data = await res.json();
-    req.user = data.user;
-    return NextResponse.next();
-  } else {
+  if (!response.ok) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_API_URL}/login`);
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
