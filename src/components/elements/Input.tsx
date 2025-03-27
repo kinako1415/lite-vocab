c86a4e26c1dd3c955d60bb8efde6c208d01ed768
kinako1415 <Owata09@outlook.jp>
2025-03-28 00:24:54 +0900

    目隠し機能の作成

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,30 +1,99 @@
+// import { useState } from "react";
+// import styles from "./Input.module.scss";
+// import Image from "next/image";
+
+// const StartButton = ({
+//   url,
+//   onChange,
+//   type,
+//   placeholder,
+// }: {
+//   url?: string;
+//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+//   type?: string;
+//   placeholder: string;
+// }) => {
+//   const [isShow, setIsShow] = useState<boolean>(true);
+
+//   return (
+//     <div className={styles.inputContainer}>
+//       <input
+//         className={styles.input}
+//         type={type}
+//         placeholder={placeholder}
+//         onChange={(e) => {
+//           onChange(e);
+//         }}
+//       ></input>
+//       <Image
+//         src={url ? url : ""}
+//         alt="book"
+//         width={24}
+//         height={24}
+//         className={styles.image}
+//       />
+//     </div>
+//   );
+// };
+
+// export default StartButton;
+
+import { useState } from "react";
 import styles from "./Input.module.scss";
 import Image from "next/image";
 
 const StartButton = ({
   url,
   onChange,
-  type,
+  type = "text",
+  placeholder,
+  isPassword = false,
 }: {
-  url: string;
+  url?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  type: string;
+  type?: string;
+  placeholder: string;
+  isPassword?: boolean;
 }) => {
+  const [isHidden, setIsHidden] = useState<boolean>(true);
+
   return (
     <div className={styles.inputContainer}>
-      <input
-        className={styles.input}
-        type={type}
-        onChange={(e) => {
-          onChange(e);
-        }}
-      ></input>
+      {isPassword ? (
+        <input
+          className={styles.input}
+          type={isHidden ? "password" : "text"}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e);
+          }}
+        />
+      ) : (
+        <input
+          className={styles.input}
+          type={type}
+          placeholder={placeholder}
+          onChange={(e) => {
+            onChange(e);
+          }}
+        />
+      )}
       <Image
-        src={url}
-        alt="book"
+        src={
+          isPassword
+            ? isHidden
+              ? "https://api.iconify.design/tabler:eye-closed.svg?color=%23A4A5B5"
+              : "https://api.iconify.design/tabler:eye.svg?color=%23A4A5B5"
+            : url
+            ? url
+            : ""
+        }
+        alt="toggle visibility"
         width={24}
         height={24}
         className={styles.image}
+        onClick={isPassword ? () => setIsHidden(!isHidden) : () => {}} // クリックで状態を切り替え
+        style={{ cursor: "pointer" }} // クリックできるように
       />
     </div>
   );
