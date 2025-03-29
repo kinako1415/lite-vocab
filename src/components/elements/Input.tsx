bb56d883616a35d8365907fd5d9be371882a3ed8
kinako1415 <Owata09@outlook.jp>
2025-03-29 18:09:04 +0900

    inputの作成

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -1,7 +1,19 @@
 import styles from "./Input.module.scss";
+import Image from "next/image";
 
-const StartButton = () => {
-  return <input className={styles.input} />;
+const StartButton = ({ url }: { url: string }) => {
+  return (
+    <div className={styles.inputContainer}>
+      <input className={styles.input} />
+      <Image
+        src={url}
+        alt="book"
+        width={24}
+        height={24}
+        className={styles.image}
+      />
+    </div>
+  );
 };
 
 export default StartButton;
