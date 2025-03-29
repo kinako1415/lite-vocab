e1381a3f49641b7bf9020f0e668832554d76b457
kinako1415 <Owata09@outlook.jp>
2025-03-29 18:09:04 +0900

    protectedの作成

---
+++ src/lib/sendRequestWithToken .ts
@@ -0,0 +1,19 @@
+import getIdTokenAuth from "./getIdTokenAuth";
+
+const sendRequestWithToken = async () => {
+  const token = await getIdTokenAuth();
+
+  if (!token) {
+    console.error("No token available");
+    return;
+  }
+
+  const response = await fetch("/api/protected", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await response.json();
+};
