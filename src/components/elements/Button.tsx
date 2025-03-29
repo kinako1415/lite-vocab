92a4895351c846cc11ab1d07e56a6d900ba0b746
kinako1415 <Owata09@outlook.jp>
2025-03-29 18:09:04 +0900

    formの作成

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -4,14 +4,16 @@ const Button = ({
   children,
   type,
   onClick,
+  color,
 }: {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
+  color?: "gray";
 }) => {
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${color ? styles[color] : ""}`}
       type={type}
       onClick={() => {
         if (onClick) onClick();
