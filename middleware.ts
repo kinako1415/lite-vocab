ec66e6c12bddf003ab22531cfc32fed52a24d1c0
kinako1415 <Owata09@outlook.jp>
2025-03-29 18:09:03 +0900

    ログインページの作成

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { verifyIdToken } from "@/lib/firebaseAdmin";
+
+export async function middleware(req: NextRequest) {
+  console.log("Middleware running");
+  const token = req.cookies.get("token")?.value || null;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  try {
+    await verifyIdToken(token);
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
