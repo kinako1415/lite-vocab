9523dd0d72025471f76a762e323bbb8037bf591e
kinako1415 <Owata09@outlook.jp>
2025-05-05 23:38:01 +0900

    wordsCardのスタイル修正

--- src/components/page/WordsContent.tsx
+++ src/components/page/WordsContent.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import styles from "./WordsContent.module.scss";
 import { useAtomValue } from "jotai";
 import { activeBoxesAtom, boxesAtom } from "@/store/boxesAtom";
-import { Button } from "../elements/Button";
 import { wordsCacheAtom } from "@/store/wordsAtom";
 import { WordsModal } from "../WordsModal";
 import { IconButton } from "../elements/IconButton";
+import { WordsCard } from "./WordsCard";
 
 export const WordsContent: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -34,9 +34,7 @@ export const WordsContent: React.FC = () => {
           <div className={styles.buttonContainer}>
             {wordsCache[activeBoxes]?.map((data, i) => {
               return (
-                <Button color="gray" key={i}>
-                  {data.meaning}
-                </Button>
+                <WordsCard meaning={data.meaning} word={data.word} key={i} />
               );
             })}
           </div>
