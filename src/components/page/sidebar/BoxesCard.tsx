fe1593cb2f30eb660e6ba2841aff88b4c43c85d0
kinako1415 <Owata09@outlook.jp>
2025-05-11 23:48:19 +0900

    buttonAnimationの修正

--- src/components/page/sidebar/BoxesCard.tsx
+++ src/components/page/sidebar/BoxesCard.tsx
@@ -33,58 +33,73 @@ export const BoxesCard = ({
   };
 
   return (
-    <div className={styles.mainContainer}>
-      <motion.button
-        key={`button-${isActive}`}
-        className={`${styles.button} ${isActive && styles.active}`}
-        whileTap={{ scale: 0.98 }}
-        onClick={() => {
-          if (onClick) onClick();
-        }}
-        onHoverStart={() => setIsHovered(true)}
-        onHoverEnd={() => setIsHovered(false)}
-      >
-        {children}
-
+    <div className={styles.container}>
+      <div className={styles.highlightBarContainer}>
         <AnimatePresence initial={false}>
           <motion.div
             className={styles.highlightBar}
             initial={{ scale: 0, y: "-50%" }}
-            animate={{ scale: isHovered ? 1 : 0, y: "-50%" }}
-            whileHover={{ scale: 1, y: "-50%" }}
-            transition={{ duration: 0.01 }}
+            animate={{
+              scale: isActive ? 1 : isHovered ? 1 : 0,
+              scaleY: isActive ? 2 : 1,
+              y: "-50%",
+            }}
+            whileHover={{
+              scale: 1,
+              scaleY: isActive ? 2 : 1,
+              y: "-50%",
+            }}
+            transition={{
+              duration: 0.01,
+              ease: "easeInOut",
+            }}
           />
         </AnimatePresence>
-      </motion.button>
-      <AnimatePresence initial={false}>
-        {isActive && (
-          <motion.div
-            key="button-container"
-            className={styles.buttonContainer}
-            initial={{ height: 0, opacity: 1, marginTop: 0 }}
-            animate={{ height: "auto", opacity: 1, marginTop: 8 }}
-            exit={{ height: 0, opacity: 1, marginTop: 0 }}
-            transition={{ duration: 0.1 }}
-          >
-            <IconButton
-              url="https://api.iconify.design/heroicons:play-solid.svg?color=%23ffffff"
-              color="purple"
-            />
-            <IconButton
-              url="https://api.iconify.design/material-symbols:delete-outline.svg?color=%23ffffff"
-              color="purple"
-              onClick={() => setIsDeleteModalOpen(true)}
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
+      </div>
+      <div className={styles.mainContainer}>
+        <motion.button
+          key={`button-${isActive}`}
+          className={`${styles.button} ${isActive ? styles.active : ""}`}
+          whileTap={{ scale: 0.98 }}
+          onClick={() => {
+            if (onClick) onClick();
+          }}
+          onHoverStart={() => setIsHovered(true)}
+          onHoverEnd={() => setIsHovered(false)}
+        >
+          {children}
+        </motion.button>
 
-      <DeleteModal
-        isOpen={isDeleteModalOpen}
-        setIsOpen={setIsDeleteModalOpen}
-        onDelete={handleDelete}
-        boxName={boxName}
-      />
+        <AnimatePresence initial={false}>
+          {isActive && (
+            <motion.div
+              key="button-container"
+              className={styles.buttonContainer}
+              initial={{ height: 0, opacity: 1, marginTop: 0 }}
+              animate={{ height: "auto", opacity: 1, marginTop: 8 }}
+              exit={{ height: 0, opacity: 1, marginTop: 0 }}
+              transition={{ duration: 0.1 }}
+            >
+              <IconButton
+                url="https://api.iconify.design/heroicons:play-solid.svg?color=%23ffffff"
+                color="purple"
+              />
+              <IconButton
+                url="https://api.iconify.design/material-symbols:delete-outline.svg?color=%23ffffff"
+                color="purple"
+                onClick={() => setIsDeleteModalOpen(true)}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+        <DeleteModal
+          isOpen={isDeleteModalOpen}
+          setIsOpen={setIsDeleteModalOpen}
+          onDelete={handleDelete}
+          boxName={boxName}
+        />
+      </div>
     </div>
   );
 };
